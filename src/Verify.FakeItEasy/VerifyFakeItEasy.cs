50cde1690f9b33b52e93a0b6706ed070b4ac936d
Simon Cropp <simon.cropp@gmail.com>
2022-12-19 20:50:32 +1100

    update to Verify 19.5.0

--- src/Verify.FakeItEasy/VerifyFakeItEasy.cs
+++ src/Verify.FakeItEasy/VerifyFakeItEasy.cs
@@ -2,10 +2,13 @@
 
 public static class VerifyFakeItEasy
 {
-    public static void Enable() =>
+    public static void Enable()
+    {
+        InnerVerifier.ThrowIfVerifyHasBeenRun();
         VerifierSettings.AddExtraSettings(serializer =>
         {
             serializer.Converters.Add(new CallConverter());
             serializer.Converters.Add(new FakeConverter());
         });
+    }
 }
