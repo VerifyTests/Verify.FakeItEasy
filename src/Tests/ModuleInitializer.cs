6a8f93996d68d038b50147ab588ea1b3a468cc6f
Simon Cropp <simon.cropp@gmail.com>
2023-02-07 21:47:11 +1100

    implement initialize pattern

--- src/Tests/ModuleInitializer.cs
+++ src/Tests/ModuleInitializer.cs
@@ -4,11 +4,11 @@
 
     [ModuleInitializer]
     public static void Init() =>
-        VerifyFakeItEasy.Enable();
+        VerifyFakeItEasy.Initialize();
 
     #endregion
 
     [ModuleInitializer]
     public static void InitOther() =>
-        VerifyDiffPlex.Initialize();
+        VerifierSettings.InitializePlugins();
 }
