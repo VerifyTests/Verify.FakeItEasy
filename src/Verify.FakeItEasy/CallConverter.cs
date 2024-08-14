39953785d72e25fdff8656e81f63e7bfedf5cbbd
Simon Cropp <simon.cropp@gmail.com>
2024-08-15 07:51:27 +1000

    .

--- src/Verify.FakeItEasy/CallConverter.cs
+++ src/Verify.FakeItEasy/CallConverter.cs
@@ -5,12 +5,5 @@ class CallConverter :
 {
     public override void Write(VerifyJsonWriter writer, ICompletedFakeObjectCall call)
     {
-        var target = new CallTarget
-        (
-            call.Method,
-            call.Arguments,
-            call.ReturnValue
-        );
-        writer.Serialize(target);
     }
 }
--- src/Verify.FakeItEasy/CallTarget.cs
+++
@@ -1,4 +0,0 @@
-using FakeItEasy.Configuration;
-// ReSharper disable NotAccessedPositionalProperty.Global
-
-record CallTarget(MethodInfo Method, ArgumentCollection Arguments, object? Return);
